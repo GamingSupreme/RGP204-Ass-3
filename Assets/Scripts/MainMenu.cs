94ce02c9332f2cf613d96c7b7520aa9d5e04240c
leylasimone <33478255+leylasimone@users.noreply.github.com>
2022-07-30 22:52:37 +1000

    Sound implementation

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,13 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void Start()
+    {
+        // Sound
+        FindObjectOfType<AudioManager>().Play("MenuMusic");
+        //Sound
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // Sound
+        FindObjectOfType<AudioManager>().Play("MenuButton");
+        //Sound
     }
     public void QuitGame()
     {
         Debug.Log("The app has been terminated. Next!");
+        // Sound
+        FindObjectOfType<AudioManager>().Play("MenuButton");
+        //Sound
         Application.Quit();
     }
 }
