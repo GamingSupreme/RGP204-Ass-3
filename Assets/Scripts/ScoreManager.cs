f6742c26023c654435b8a770ae76e39396e70f34
William Hendarto <40332462+Damstructions@users.noreply.github.com>
2022-07-30 18:55:43 +0700

    Updated to work with Text Mesh Pro!

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
 
-    public Text scoreText;
+    public TextMeshProUGUI scoreText;
     //public Text highscoreText;
 
     int score = 0;
